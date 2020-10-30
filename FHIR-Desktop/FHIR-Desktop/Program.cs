2e2c8b56aa0394082c2b455b01fbea9759b4801d
Daryl Moehrke <daryl.moehrke@spauldingmedical.com>
2020-10-30 16:18:04 -0500

    Made a quick test
    Mostly to check if my understanding of how Timer's worked at a basic level was correct

--- FHIR-Desktop/FHIR-Desktop/Program.cs
+++ FHIR-Desktop/FHIR-Desktop/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace FHIR_Desktop
 {
     class Program
     {
+        private static Timer mainTimer;
+
         static void Main(string[] args)
         {
+            mainTimer = new Timer(1000*60); //Once every minuite
+            mainTimer.Elapsed += OnTimer;
+            mainTimer.AutoReset = true;
+            mainTimer.Enabled = true;
+            Console.ReadLine();
+        }
+
+        private static void OnTimer(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Event Triggered");
         }
     }
 }
