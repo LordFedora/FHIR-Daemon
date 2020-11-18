3eaec5ac9dc2babf0f361c8b98119a874f50299d
Daryl Moehrke <daryl.moehrke@spauldingmedical.com>
2020-11-18 17:26:07 -0600

    Adding Content to Toasts
    Old Code that i forgot to commit, but now pulls content from a server, parses it (poorly) and adds it to the toast body

--- FHIR-App/FHIR-App/MainWindow.xaml.cs
+++ FHIR-App/FHIR-App/MainWindow.xaml.cs
@@ -12,10 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.IO;
 using System.Timers;
 using Microsoft.Toolkit.Uwp.Notifications;
 using Windows.UI;
 using Windows.UI.Notifications;
+using Newtonsoft.Json;
 
 namespace FHIR_App
 {
@@ -25,12 +28,32 @@ namespace FHIR_App
     public partial class MainWindow : Window
     {
         static private Timer mainLoopTimer;
+        static private String BaseAPIURL = "http://test.fhir.org/r2/AuditEvent/1/_history/1?_format=json";
+        //http://test.fhir.org/r2/AuditEvent/_search?_lastUpdated=gt2020-11-06T21:52:30.300Z&_sort=_lastUpdated&_format=json&_count=10
+
 
         public MainWindow()
         {
             InitializeComponent();
             DesktopNotificationManagerCompat.RegisterAumidAndComServer<ToastNotificationActivator>("BIA.FHIR_DEAMON");
             DesktopNotificationManagerCompat.RegisterActivator<ToastNotificationActivator>();
+
+            Microsoft.Win32.RegistryKey key;
+            key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("BIA");
+            key.SetValue("BIA_SERVER_NAME_1", "http://test.fhir.org/r2");
+            DateTime timestamp;
+            dynamic value = key.GetValue("BIA_LAST_UPDATED_1");
+            if (value is null)
+            {
+                key.SetValue("BIA_LAST_UPDATED_1", -1);
+                timestamp = DateTime.FromFileTimeUtc(1);
+            }
+            else
+            {
+                timestamp = DateTime.FromFileTimeUtc(value);
+            }
+            key.Close();
+
 
             mainLoopTimer = new Timer(1000 * 30);
             mainLoopTimer.AutoReset = true;
@@ -40,9 +63,25 @@ namespace FHIR_App
 
         private static void onTimerElapsed(Object source, ElapsedEventArgs e)
         {
+
+            String content = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BaseAPIURL);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                content = reader.ReadToEnd();
+            }
+
+
+            dynamic temp = JsonConvert.DeserializeObject(content);
+
+            String text = temp?.text?.div;
+
             ToastContent toastContent = new ToastContentBuilder()
                 //.AddToastActivationInfo("action=viewConversation&conversationId=5", ToastActivationType.Foreground)
-                .AddText("Hello World!")
+                .AddText(text)
                 .SetToastScenario(ToastScenario.Reminder)
                 .GetToastContent();
             
